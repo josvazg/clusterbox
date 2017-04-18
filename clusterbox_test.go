190b42fcfe641586965a3e0ca898c8278c871048
Jose Vazquez <jose.vazquez@canonical.com>
2017-04-18 10:25:19 +0200

    Add first basic unit test

---
+++ clusterbox_test.go
@@ -0,0 +1,43 @@
+package clusterbox_test
+
+import (
+	"testing"
+
+	"github.com/josvazg/clusterbox"
+)
+
+func dieOnError(t *testing.T, err error) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+type EmptyNode struct{}
+
+func (en *EmptyNode) Endpoint() string {
+	return "empty"
+}
+
+func (en *EmptyNode) Setup(nodes []clusterbox.Node) {}
+
+func (en *EmptyNode) Serve() {}
+
+func (en *EmptyNode) Client() {}
+
+func (en *EmptyNode) Stop() error {
+	return nil
+}
+
+func NewEmptyNode(i int) (clusterbox.Node, error) {
+	return &EmptyNode{}, nil
+}
+
+var sizes = []int{1, 10, 100}
+
+func TestClusterBoxWithEmptyNode(t *testing.T) {
+	for _, size := range sizes {
+		clusterbox, err := clusterbox.NewClusterBox(size, NewEmptyNode)
+		dieOnError(t, err)
+		clusterbox.Run()
+	}
+}
